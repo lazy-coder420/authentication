d902b6ad1770c09f9e7e86557e28534e9ba73d04
lazy-coder420 <mjmunna692@gmail.com>
2026-04-23 20:22:48 +0600

    it's ok data will go mongodb database

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -4,7 +4,7 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient("process.env.MDB_URI");
+const client = new MongoClient(process.env.MDB_URI);
 const db = client.db();
 
 export const auth = betterAuth({
